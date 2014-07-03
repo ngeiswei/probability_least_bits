ecced309d544837d464f0c2dd666b629bac292d8
Nil Geisweiller <ngeiswei@gmail.com>
2014-07-03 16:52:21 +0300

    Better function naming

--- probability_least_bits.hs
+++ probability_least_bits.hs
@@ -16,26 +16,26 @@ main = do
       t = read t_str :: Int
       p = read p_str :: CReal
   putStrLn (format "Probability of having at least {0} 1-bits amonst {1} = {2}"
-            [t_str, n_str, (show (patleast n t p))])
+            [t_str, n_str, (show (p_atleast n t p))])
   putStrLn ("Probability of the universe " ++ 
             "(should be 1.0, otherwise there is a problem) = " ++
-            (show (puniverse n t p)))
+            (show (p_universe n t p)))
 
 -- Probability of having exactly k 1-bits:
 --
--- pexact(n, k, p) = C(n, k) * p^k * p^(n-k)
-pexact :: Int -> Int -> CReal -> CReal
-pexact n k p = (p**n1 * (1-p)**n0) * (fromIntegral (choose n k))
+-- p_exact(n, k, p) = C(n, k) * p^k * p^(n-k)
+p_exact :: Int -> Int -> CReal -> CReal
+p_exact n k p = (p**n1 * (1-p)**n0) * (fromIntegral (choose n k))
   where n1 = fromIntegral k
         n0 = fromIntegral (n-k)
 
 -- Probability of having at least t 1-bits:
 --
--- pcomb(n, t, p) = Sum_{k=t}^n pexact(n, k, p)
-patleast :: Int -> Int -> CReal -> CReal
-patleast n t p = sum [pexact n k p | k <- [t..n]]
+-- p_atleast(n, t, p) = Sum_{k=t}^n p_exact(n, k, p)
+p_atleast :: Int -> Int -> CReal -> CReal
+p_atleast n t p = sum [p_exact n k p | k <- [t..n]]
 
 -- Probability of the universe (to check the inaccuracies of the
 -- computation)
-puniverse :: Int -> Int -> CReal -> CReal
-puniverse n t p = sum [pexact n k p | k <- [0..n]]
+p_universe :: Int -> Int -> CReal -> CReal
+p_universe n t p = sum [p_exact n k p | k <- [0..n]]
