383cce63ef6c5f7cc75eeb3b5668c7d548ac03dd
Nil Geisweiller <ngeiswei@gmail.com>
2014-07-03 16:54:57 +0300

    Improve comments

--- probability_least_bits.hs
+++ probability_least_bits.hs
@@ -6,7 +6,7 @@ import Data.Number.CReal
 import Text.Format
 
 -- Little script to compute the probability of having at least t 1s in
--- a n-bits string, given that the probability of having 1 in any of
+-- a n-bit string, given that the probability of having 1 in any of
 -- the bit is p.
 
 main :: IO ()
@@ -15,13 +15,13 @@ main = do
   let n = read n_str :: Int
       t = read t_str :: Int
       p = read p_str :: CReal
-  putStrLn (format "Probability of having at least {0} 1-bits amonst {1} = {2}"
+  putStrLn (format "Probability of having at least {0} 1s in a {1}-bit string = {2}"
             [t_str, n_str, (show (p_atleast n t p))])
   putStrLn ("Probability of the universe " ++ 
             "(should be 1.0, otherwise there is a problem) = " ++
             (show (p_universe n t p)))
 
--- Probability of having exactly k 1-bits:
+-- Probability of having exactly k 1s in a n-bit string:
 --
 -- p_exact(n, k, p) = C(n, k) * p^k * p^(n-k)
 p_exact :: Int -> Int -> CReal -> CReal
@@ -29,7 +29,7 @@ p_exact n k p = (p**n1 * (1-p)**n0) * (fromIntegral (choose n k))
   where n1 = fromIntegral k
         n0 = fromIntegral (n-k)
 
--- Probability of having at least t 1-bits:
+-- Probability of having at least t 1s in a n-bit string:
 --
 -- p_atleast(n, t, p) = Sum_{k=t}^n p_exact(n, k, p)
 p_atleast :: Int -> Int -> CReal -> CReal
